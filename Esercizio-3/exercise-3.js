1568b2ae599983d224adcfdaf59911a6a4c6f680
GiuseppeMeliDevelhope <115168393+GiuseppeMeliDevelhope@users.noreply.github.com>
2022-12-14 01:39:36 +0100

    Add files via upload

---
+++ Esercizio-3/exercise-3.js
@@ -0,0 +1,24 @@
+const sum =  (a, b) => a+b;
+ 
+
+const subtract =  (a, b) => a-b;
+ 
+
+const multiply =  (a, b) => a*b;
+
+
+const divide =  (a, b) => a/b;
+
+
+const finalSum  = () => {
+  return divide(subtract(multiply(sum(2, 4), sum(5, 2)), 2), 5);
+}
+
+
+const log = function (value) {
+  console.log(value);
+}
+
+
+log(finalSum());
+
