76122421a10ab490e34c5871b73365e47487b06b
GiuseppeMeliDevelhope <115168393+GiuseppeMeliDevelhope@users.noreply.github.com>
2022-12-14 15:21:15 +0100

    Add files via upload

---
+++ Esercizio-9/exercise-1.js
@@ -0,0 +1,11 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+// Print values of person using Object.keys
+
+console.log(Object.keys(person));  //printed keys
+console.log(Object.values(person)); //printed values
+console.log(Object.entries(person)); //printed both keys and values
