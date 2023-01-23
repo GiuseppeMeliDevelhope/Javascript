281fb4cc4993077eec15de832c3d11087323b59e
GiuseppeMeliDevelhope <115168393+GiuseppeMeliDevelhope@users.noreply.github.com>
2023-01-23 01:10:19 +0100

    Add files via upload

---
+++ Esercizio-39/exercise.js
@@ -0,0 +1,35 @@
+const isLogged = true;
+
+
+function logIn(isLogged){
+   
+    return new Promise((resolve,reject)=>{
+        let randNum=Math.random();
+        if(isLogged===true) {
+            resolve(randNum);
+        }else{
+            reject(new Error("Not logged in!"))
+        }
+    })
+}
+
+
+
+function person(randNum){
+    return new Promise((resolve,reject)=>{
+        
+            if(randNum>0.5){
+                resolve({name:"John",age:24})
+            }else{
+                reject(new Error("There is an error"))
+            }
+
+        })
+    }
+
+
+logIn(isLogged)
+  .then(person)
+  .then((val)=> console.log(val))
+  .catch ((err)=> console.log(err))
+  .finally(()=> console.log("Finished"));
